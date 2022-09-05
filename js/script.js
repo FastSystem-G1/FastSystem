64844c486e45a0ff32f7c86e1b09192e4a69b624
FelipeGomes-20 <felipe.gomes@sptech.school>
2022-09-04 22:53:50 -0300

    links da tela home atualizados

--- js/script.js
+++ js/script.js
@@ -11,3 +11,15 @@ function Cadastrar(){
     }
 
 }
+
+function clickmenu() {
+
+    if (itens.style.display == 'block') {
+        itens.style.display = 'none';
+    }
+    else {
+        itens.style.display = 'block';
+    }
+
+
+}
